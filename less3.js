450c80e6feea4b2ea01936fa67b47789d60b0b1d
razak <ipoxe@mail.ru>
2022-02-28 17:53:16 +0300

    загрузил корректный файл по домашнему заданию №3

--- less3.js
+++ less3.js
@@ -1,97 +1,111 @@
-'use strict';
+"use strict";
 
 // Задача 1
 
-//Пример 1
-let a = 1, b = 1, c, d;
-c = ++a;
-alert(c);
-// Ответ: Инкремент. Дает a = 1 + 1, т.к. c = a, то с,a = 2.
+let values = 10;
 
-//Пример 2
-d = b++;
-alert(d);
-// Ответ: b++ постфиксная форма дикремента. b = 1 + 1, т.к. d = b запишет 1 в d
-
-//Пример 3
-c = 2 + ++a;
-alert(c);
-// Ответ: Т.к. на данный момент в С записано уже 2 из примера 1, то прибавляем 2 и 1 из ++a
-
-// //Пример 4
-d = 2 + b++;
-alert(d);
-
-alert(a);
-alert(b);
-// Ответ: alert(d) d содержи 1 из примера 2. 2 + 1 (b++) + 1( который в d ) = 4
-// alert(a) ответ 3 так как записаны данные из 1 и 3 го примера.
-// alert(b) ответ 3 так как записаны данные из 1, 2 и 4 примера 
-
+for (let i = 0; i <= values; i++) {
+  if (i == 0) {
+    console.log(i + "– ноль это ноль");
+  } else if (i % 2 == 0) {
+    console.log(i + "– четное число");
+  } else {
+    console.log(i + "– нечетное число");
+  }
+}
 
 // Задача 2
 
-let a = 2;
-let x = 1 + (a *=2);
-//Ответ 5. 1 + (a = a * 2);
-//(a *=2) это сокращенная запись a = a * 2
-
+const post = {
+  author: "John",
+  postId: 23,
+  comments: [
+    {
+      userId: 10,
+      userName: "Alex",
+      text: "lorem ipsum",
+      rating: {
+        likes: 10,
+        dislikes: 2,
+      },
+    },
+    {
+      userId: 5,
+      userName: "Jane",
+      text: "loren ipsum 2",
+      rating: {
+        likes: 3,
+        dislikes: 1,
+      },
+    },
+  ],
+};
+console.log(post.author);
+console.log(post.comments[0].rating["dislikes"]);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
 
 //Задача 3
 
-// Введенные данные используются в 3, 4 и 5 задачах.
-let a = Number(prompt('Введите первое число'));
-let b = Number(prompt('Введите второе число'));
+const products = [
+  {
+    id: 3,
+    price: 200,
+  },
+  {
+    id: 4,
+    price: 900,
+  },
+  {
+    id: 1,
+    price: 1000,
+  },
+];
 
-if (a && b >=0) {
-alert (a - b);
-} else if (a && b < 0) {
-    alert (a * b);
-}else{
-    alert('Вы ввели не число');
-}
+let percent = 15;
+let numb_percent = (products[0].price / 100) * percent;
+products.forEach((element) => console.log(element.price - numb_percent));
 
 //Задача 4
 
-function sumNum1 (a, b) {
-    return a * b;
+const products = [
+  {
+    id: 3,
+    price: 127,
+    photos: ["1.jpg", "2.jpg"],
+  },
+  {
+    id: 5,
+    price: 499,
+    photos: [],
+  },
+  {
+    id: 10,
+    price: 26,
+    photos: ["3.jpg"],
+  },
+  {
+    id: 8,
+    price: 78,
+  },
+];
+
+//Сортировка по наличию фото
+const productsSearch = products.filter(function (products) {
+  return products.photos && products.photos.length > 0;
+});
+console.log(productsSearch);
+
+//Сортировка по цене
+const productsSortPrice = products.sort(function priceSort(price1, price2) {
+  return price1.price - price2.price;
+});
+console.log(productsSortPrice);
+
+// Задача 5
+for (let i = 0; i < 9; console.log(i++)) {}
+
+//Задача 6
+for (let x = "x"; x.length <= 20; x += "x") {
+  console.log(x);
 }
-
-function sumNum2 (a, b) {
-    return a / b;
-}
-
-function sumNum3 (a, b) {
-    return a + b;
-}
-
-function sumNum4 (a, b) {
-    return a - b;
-}
-
-console.log (sumNum1 (a, b));
-console.log (sumNum2 (a, b));
-console.log (sumNum3 (a, b));
-console.log (sumNum4 (a, b));
-
-//Задача 5
-
-function mathOperation( arg1, arg2,operation) {
-
-    switch(operation) {
-        case '*':
-            return(arg1, arg2);
-        case '/':
-            return(arg1, arg2);
-        case '+':
-            return(arg1, arg2);
-        case '-':
-            return(arg1, arg2);
-        default:
-            'Ошибка';
-    }
-}
-alert (mathOperation(a,b, '*'));
-alert (mathOperation(a,b, '/'));
-alert (mathOperation(a,b, '+'));
-alert (mathOperation(a,b, '-'));
